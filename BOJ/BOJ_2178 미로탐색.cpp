b9d254de5e34796f00d5434a45156b08387c9b38
김준호 <kimjunho1231@naver.com>
2023-05-07 14:46:48 +0900

    C++ BOJ

--- BOJ/BOJ_2178 미로탐색.cpp
+++ BOJ/BOJ_2178 미로탐색.cpp
@@ -1,70 +1,89 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 
-void Search_Maze(bool** Maze,bool** visited,int x,int y,int N,int M)
+void Search_Maze(bool** Maze, bool** visited, int x, int y, int N, int M)
 {
-	int dx[4] = { 0,1,0,-1 };
-	int dy[4] = { 1,0,-1,0 };
+	int dx[4] = { 1,0,0,-1 };
+	int dy[4] = { 0,1,-1,0 };
+	static int cnt = 1;
+
+	if (x == N - 1 && y == M - 1)
+	{
+		cout <<cnt;
+		return;
+	}
 
 	for (int i = 0; i < 4; i++)
 	{
-		if (Maze[x][y] == 1)
+		if (x + dx[i] == 0 || y + dy[i] == 0 || x + dx[i] == N || y + dy[i] == M)
 		{
-
+			continue;
+			//벽으로 가면 못가게하기
 		}
-		else if (Maze[x][y]==0&&)
+		else if (visited[x + dx[i]][y + dy[i]]) {
+			continue;
+			//방문했던 곳이라면
+		}
+		else if (Maze[x + dx[i]][y + dy[i]] == 1 && visited[x + dx[i]][y + dy[i]] == 0)
+		{
+			visited[x + dx[i]][y + dy[i]] = 1;
+			cnt++;
+			Search_Maze(Maze, visited, x + dx[i], y + dy[i], N, M);
+		}
+		else {
+			continue;
+		}
 	}
-	if (x == 0 || y == 0 || x == N || y == M)
-	{
-
-	}
+	cnt--;
 }
+
+
 int main()
 {
-	int N, M;
+	int N, M,count=0;
+	string s;
+	char q;
 	cin >> N >> M;
-	bool** Maze = new bool*[N + 2];
-	for (int i = 0; i <= N+1; i++)
+	bool** Maze = new bool* [N + 2];
+	for (int i = 0; i <= N + 1; i++)
 	{
 		Maze[i] = new bool[M + 2];
 	}
-
+	string temp;
 	for (int i = 1; i <= N; i++)
 	{
+		count = 0;
+		cin >> temp;
 		for (int j = 1; j <= M; j++)
 		{
-			cin >> Maze[i][j];
+			Maze[i][j] = temp[count] - '0';
+			count++;
 		}
+		temp = "";
 	}
 
-	for (int i = 0; i <= N+1; i++)
+	for (int i = 0; i <= N + 1; i++)
 	{
 		Maze[i][0] = 0;
 	}
-	for (int j = 0; j <= M+1; j++)
+	for (int j = 0; j <= M + 1; j++)
 	{
 		Maze[0][j] = 0;
 	}
 
-	for (int i = 1; i <= N+1; i++)
+	for (int i = 1; i <= N + 1; i++)
 	{
-		Maze[i][M+1] = 0;
+		Maze[i][M + 1] = 0;
 	}
-	for (int i = 1; i <= M+1; i++)
+	for (int i = 1; i <= M + 1; i++)
 	{
-		Maze[N+1][i] = 0;
+		Maze[N + 1][i] = 0;
 	}
-	for (int i = 0; i <= N+1; i++)
-	{
-		for (int j = 0; j <= M+1; j++)
-		{
-			cout << Maze[i][j] << " ";
-		}
-		cout << endl;
-	}
+
 	static bool** visited = new bool* [N + 2];
-	for (int i = 0; i <= N + 1;i++)
+	for (int i = 0; i <= N + 1; i++)
 	{
 		visited[i] = new bool[M + 2];
 	}
@@ -76,9 +95,10 @@ int main()
 			visited[i][j] = 0;
 		}
 	}
-	Search_Maze(Maze,visited, 1, 1, N + 1, M + 1);
+	visited[1][1] = 1;
+	Search_Maze(Maze, visited, 1, 1, N + 1, M + 1);
 
-	for (int i = 0; i <= N+1; i++)
+	for (int i = 0; i <= N + 1; i++)
 	{
 		delete[] Maze[i];
 	}
