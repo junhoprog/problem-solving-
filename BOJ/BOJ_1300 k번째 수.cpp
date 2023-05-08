b9a33f189ccc6ab9b893881bbe0a040d571c68fc
김준호 <kimjunho1231@naver.com>
2023-05-08 23:46:50 +0900

    C++ BOJ

--- BOJ/BOJ_1300 k번째 수.cpp
+++ BOJ/BOJ_1300 k번째 수.cpp
@@ -1,5 +1,4 @@
 /*
-메모리 초과
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -41,4 +40,44 @@ int main()
 	delete[]A;
 	return 0;
 }
+*/
 
+#include<iostream>
+#include<algorithm>
+using namespace std;
+long long N, k,cnt=0,law,high,mid;
+
+long long count(long long x)
+{
+	long long sum = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		sum += min(x / i, N);
+	}
+	return sum;
+}
+long long solve(long long N, long long k)
+{
+	law = 1; high = N*N;
+	while (law <= high)
+	{
+		mid = (law + high) / 2;
+		cnt = count(mid);
+		if (k <= cnt)
+		{
+		high = mid - 1;
+		}
+		else{
+			law = mid + 1;
+		}
+	
+	}
+	return law;
+}
+int main()
+{
+
+cin >> N >> k;
+cout<<solve(N, k);
+return 0;
+}
