3a53e72ccdc89a25e2318f0d209511c588c53cd3
김준호 <kimjunho1231@naver.com>
2023-05-16 10:29:02 +0900

    C++ BOJ

---
+++ BOJ/BOJ_10811 바구니뒤집기.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+int main()
+{
+	int N, M, a, b, t = 0,temp;
+	cin >> N >> M;
+	vector<int>v(N+1);
+	for (int i = 0; i < v.size(); i++)
+	{
+		v[i] = i;
+	}
+
+	for (int i = 0; i < M; i++)
+	{
+		t = 0;
+		cin >> a >> b;
+		for (int j = a; j< b; j++)//(a+b)/2
+		{
+			temp = v[j];
+			v[j] = v[b - t];
+			v[b - t] = temp;
+
+			if (j+1>=(b-t))
+			{
+				break;
+			}
+			t++;
+		}
+	}
+	for (int i = 1; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
+	}
+	return 0;
+}
