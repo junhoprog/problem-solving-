f1e963a0ab8b793c6e1c678837638512ce3b0648
김준호 <kimjunho1231@naver.com>
2023-05-22 20:35:29 +0900

    c++ 백준

---
+++ BOJ/BOJ_1620 나는야 포켓몬마스터 이다솜.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include<vector>
+#include<map>
+#include<string>
+
+using namespace std;
+
+map<string, int>m;
+map<int, string>m2;
+vector<string>v;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	//수록되어있는 포켓몬 N, 내가 맞춰야하는 문제의 개수 M
+	//N개의 줄에 포켓몬의 번호가 1~N번 포켓몬이 한줄씩 입력으로돌어옴(모두영어 첫글자만 대문자고 나머지 소문자)
+	//일부 포켓몬은 마지막 문자만 대문자일 수 있음. 이름 최대길이 20 최소 2
+	//M개의 줄에 내가 맞춰야하는 문제 입력들어옴-> 알파벳으로 들어오면 포켓몬 번호 숫자로 들어오면 문자 출력
+
+	int N, M;
+	string s;
+
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> s;
+		m.insert({ s,i });
+		m2.insert({ i,s });
+	}
+	
+	for (int i = 0; i < M; i++)
+	{
+		cin >> s;
+		if ('0' <= s[0] && s[0] <= '9')
+		{
+			cout <<m2[stoi(s)] << '\n';//숫자라면
+			//정수
+		}
+		else {
+			cout << m[s] << "\n";
+		}
+	}
+		return 0;
+}
---
+++ BOJ/BOJ_17281 야구.cpp
@@ -0,0 +1,115 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+int n;
+vector<int>v[9];
+bool visited[9] = {0,};
+int seat[4] = { 0,0,0,0 };
+int S[9];
+int j = 0;
+int out = 0;
+int score=0;
+int M_score=0;
+int ening = 0;
+void playGame()
+{
+	static int s=0;
+	int t = 0;
+	while (1) {
+		for (int i = s; i < 9; i++)
+		{
+			if (S[i] == 0)
+			{
+
+				out++;
+				if (out == 3)
+				{
+					ening++;
+					s = i + 1;
+					t = 1;
+					break;
+				}
+			}
+
+			else if (S[i] == 1)
+			{
+				if (seat[3] == 1)
+				{
+					seat[0] = 0;
+					score++;
+				}
+				else {
+
+				}
+			}
+
+			else if (S[i] == 2)
+			{
+
+			}
+
+			else if (S[i] == 3)
+			{
+
+			}
+			else if (S[i] == 4)
+			{
+				for (int i = 1; i <= 3; i++)
+				{
+					if (seat[i] == 1)
+					{
+						seat[i] = 0;
+						score++;
+					}
+				}
+
+			}
+			
+		}
+		if (t == 1)
+		{
+			break;
+		}
+		s = 0;
+	}
+
+	M_score = max(score, M_score);
+}
+void permutation(int index)
+{
+	if (index == 9)
+	{
+		playGame();
+		//game시작
+	}
+	else {
+		for (int i = 0; i < 9; i++)
+		{
+
+			if (visited[i] == 0)
+			{
+				visited[i] = 1;
+				S[index] = v[j][i];
+				permutation(index + 1);
+				visited[i] = 0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int t = 0; t < 9; t++)
+		{
+			cin >> v[i][t];
+		}
+	}
+	S[3] = v[j][0];
+	visited[0] = 1;
+	permutation(0);
+	return 0;
+}
