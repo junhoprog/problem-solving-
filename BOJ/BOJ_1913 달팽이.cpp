5afbe26047ef7cc4439e548c2f6edaf5244a420b
김준호 <kimjunho1231@naver.com>
2023-09-18 23:10:39 +0900

    C++ BOJ

--- BOJ/BOJ_1913 달팽이.cpp
+++ BOJ/BOJ_1913 달팽이.cpp
@@ -33,6 +33,11 @@ int main()
 	int count = 0;
 	int cnt = 1;
 	map[x][y] = 1;
+	if (map[x][y] == K)
+	{
+		result_x = x + 1;
+		result_y = y + 1;
+	}
 	int num = 1;
 	//cout << x << " " << y << " " << num << endl;
 	num = 2;
@@ -40,6 +45,11 @@ int main()
 	x = x - 1;
 	int t = 1;
 	map[x][y] = num;//고정 2
+	if (num == K)
+	{
+		result_x = x + 1;
+		result_y = y + 1;
+	}
 	//cout << x << " " << y << " " << num << endl;
 	num++;
 	while (1) {
@@ -66,7 +76,7 @@ int main()
 			count++;
 			if (count == 2)
 			{
-			
+
 				//한번더 이동 후
 				x += x_move[index];
 				y += y_move[index];
@@ -102,7 +112,7 @@ int main()
 		{
 			break;
 		}
-		if (x > N - 1 || y > N - 1||x<0||y<0)
+		if (x > N - 1 || y > N - 1 || x < 0 || y < 0)
 		{
 
 			break;
