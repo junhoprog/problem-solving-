eab92387d1c4202761aaea3976e8091f3dc342fc
김준호 <kimjunho1231@naver.com>
2024-04-07 13:29:37 +0900

    C++ Programmers

---
+++ Programers/뒤에 있는 큰 수 찾기.cpp
@@ -0,0 +1,72 @@
+﻿#include <string>
+#include <vector>
+#include<stack>
+#include<iostream>
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    int n = numbers.size();
+    vector<int> answer;
+    stack<pair<int, int>>s;//numbers안 숫자와, answer의 index로 구성
+
+    
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        answer.push_back(0);
+        while (1)
+        {
+            //top에 있는거랑 numbers랑 비교하는 과정반복
+            if (i == 0)
+            {
+                break;
+            }
+            if (s.empty())
+            {
+                break;
+            }
+            if (s.top().second < numbers[i])
+            {
+                answer[s.top().first] = numbers[i];
+                s.pop();
+            }
+            else
+            {
+                break;
+            }
+        }
+        s.push(make_pair(i, numbers[i]));
+
+        if (i == numbers.size() - 1)//끝지점이면
+        {
+            while (!s.empty())
+            {
+                answer[s.top().first] = -1;
+                s.pop();
+            }
+        }
+        //스택에 그냥 다 넣는데 넣고나서 다음으로 넘어갔을 때 top이랑 비교하는 과정 반복
+        //0:9
+        //top에 있는거 비교하기
+    }
+    return answer;
+}
+int main()
+{
+    vector<int>v;
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(3);
+    v.push_back(5);
+    vector<int>v2;
+    v2 = solution(v);
+    for (int i = 0; i < v2.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+}/*
+ 
+ 절반하면 n개가 나왔다면 2*n-(0,t)구간 빼면됨
+ x,y에서
+ x가 0일때  
+ 0-> r1-r2+1개*2
+ */
---
+++ Programers/시소 짝꿍.cpp
@@ -0,0 +1,61 @@
+﻿#include <string>
+#include <vector>
+#include<map>
+#include<iostream>
+
+using namespace std;
+
+long long solution(vector<int> weights) {
+    long long answer = 0;
+    vector<long long>cnt(1001, 0);
+    for (int i = 0; i < weights.size(); i++)
+    {
+        cnt[weights[i]]++;//100 180 360 ...각 cnt에 매핑되어 개수증가 
+    }
+    for (int i = 0; i < weights.size(); i++)
+    {
+        //현재 나온 수를 비율로 따져보았을 때 나올 수 있는 것은
+        //2:3, 1:2, 3:4임
+        if (weights[i] % 2 == 0)
+        {
+            //2:3일지 확인
+            if ((weights[i] / 2) * 3 <= 1000)//cnt인덱스를 넘어가지 않게
+            {
+                answer += cnt[(weights[i] / 2) * 3];
+            }//0이면 그 상태일테고 1이상이면 그 만큼 더해질것임.
+        }
+        if (weights[i] % 3 == 0)
+        {
+            //3:4일지 확인
+            if ((weights[i] / 3) * 4 <= 1000)
+                answer += cnt[(weights[i] / 3) * 4];//0이면 그 상태일테고 1이상이면 그 만큼 더해질것임.
+        }
+
+        if (weights[i] * 2 <= 1000)
+            answer += cnt[weights[i] * 2];
+        //1:2일지 확인
+    }
+
+    for (int i = 100; i <= 1000; i++)
+    {
+        //같은 수 인 숫자가 n(n>=2)개일 경우 nC2해줘야함.
+        //n(n-1)/2
+        if (cnt[i] > 1)
+        {
+            answer += (long long)(cnt[i] * (cnt[i] - 1)) / 2;//해당 부분 개수
+
+        }
+    }
+
+    return answer;
+}
+int main()
+{
+    vector<int>v;
+    v.push_back(100);
+    v.push_back(180);
+    v.push_back(360);
+    v.push_back(100);
+    v.push_back(270);
+    cout << solution(v);
+}
