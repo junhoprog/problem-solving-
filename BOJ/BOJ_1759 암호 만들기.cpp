b0007eea13d9e8ad933e463c4504bb5c4e941036
김준호 <kimjunho1231@naver.com>
2024-04-10 21:31:59 +0900

    C++ BOJ

---
+++ BOJ/BOJ_1759 암호 만들기.cpp
@@ -0,0 +1,63 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+int j=0, m=0;
+char moeum[5] = { 'a','e','i','o','u' };
+
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+void combination(char arr[], int depth,int next, int n, int r)
+{
+	if (depth == r)
+	{
+		m = 0;
+		j = 0;
+		for (int i = 0; i < r; i++)
+		{
+			if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u')
+			{
+				m++;
+			}
+			else {
+				j++;
+			}
+		}
+		if (m >= 1 && j >= 2)
+		{
+			for (int i = 0; i < r; i++)
+			{
+				cout << arr[i];
+			}
+		
+			cout << "\n";
+		}
+		return;
+	}
+
+	for (int i = next; i <=n-r+depth; i++)//모음 최소 1개, 자음 최소 2개 포함되어야함.
+	{
+		swap(arr[i], arr[depth]);
+		combination(arr, depth + 1,i+1, n, r);
+		swap(arr[i], arr[depth]);
+	}
+}
+int main()
+{
+	int r, n;
+	char x;
+	cin >> r>>n;
+
+	char arr[15];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x;
+		arr[i] = x;
+	}
+	sort(arr, arr + n);
+	combination(arr, 0,0, n, r);
+	return 0;
+}
---
+++ BOJ/BOJ_1918 후위 표기식.cpp
@@ -0,0 +1,87 @@
+﻿#include<iostream>
+#include<stack>
+
+using namespace std;
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	string s;
+	cin >> s;
+	stack<char>str;
+	char temp;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == 'A' || s[i] == 'B' || s[i] == 'C' || s[i] == 'D' || s[i] == 'E' || s[i] == 'F' || s[i] == 'G' || s[i] == 'H' || s[i] == 'I' || s[i] == 'J' || s[i] == 'K' || s[i] == 'L' || s[i] == 'M' || s[i] == 'N' || s[i] == 'O' || s[i] == 'P' || s[i] == 'Q' || s[i] == 'R' || s[i] == 'S' || s[i] == 'T' || s[i] == 'U' || s[i] == 'V' || s[i] == 'W' || s[i] == 'X' || s[i] == 'Y' || s[i] == 'Z')
+		{
+			cout << s[i];
+			continue;
+		}
+
+		else if (s[i] == '(')
+		{
+			str.push(s[i]);
+		}
+
+		else if (s[i] == ')')
+		{
+			while (!str.empty() && str.top() != '(')
+			{
+				cout << str.top();
+				str.pop();
+			}
+			str.pop();
+		}
+
+		else if (s[i] == '+' || s[i] == '-')
+		{
+			//(가 아니면 계속 빼줘도됨
+			while (!str.empty() && str.top() !='(')
+			{
+				cout << str.top();
+				str.pop();
+			}
+			str.push(s[i]);
+		}
+
+		else if (s[i] == '*' || s[i] == '/')
+		{
+			//D+A*B*C이면 앞부분*가 더 우선순위 높아서 얘를 처리하고 다음꺼가야함
+			while (!str.empty() && (str.top() == '*' || str.top() == '/'))
+			{
+				cout << str.top();
+				str.pop();
+			}
+			str.push(s[i]);
+		}
+
+	}
+
+	while (!str.empty())
+	{
+		cout << str.top();
+		str.pop();
+	}
+	//끝났는데 아직 스택에 남아있을 수도
+
+	return 0;
+}
+//A+B*(C+D*E+F)
+//ABCDE*+F+*+
+/*
+
+*, / 일때 앞에(이면 스택에 넣음
+*,/인데앞에 ABC이면 앞에거 먼저 출력하고 *나오며
+ABCDE*+
++*(+
+
+*/
+//A,B,C->그대로출력
+//+,-는 스택에 저장
+//*,/가 나왔을 때 스택에 +,-가 있다면 출력
+//(가 나오면)가 나올때까지 다 담음
