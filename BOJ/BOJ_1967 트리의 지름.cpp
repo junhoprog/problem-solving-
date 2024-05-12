a52d6728ba556bf9e2f6a2e66b2cde18c273fc57
김준호 <kimjunho1231@naver.com>
2024-05-12 18:26:45 +0900

    C++ BOJ

---
+++ BOJ/BOJ_1967 트리의 지름.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+#include<cstring>
+#include<vector>
+using namespace std;
+
+long long int max_num = 0;
+void dfs(vector<pair<int, int>>v[], int i, long long int sum, bool visit[])
+{
+	if (visit[i]) {
+		return;
+	}
+	visit[i] = true;//방문하면 true
+	//v의 1번째부터 들어가면서 w를 더해줌 최대값을 계속 갱신함.
+	//만약 다른 방향으로 빠지게 되면 뺴주고 더해줌
+	for (int j = 0; j < v[i].size(); j++)
+	{
+		if (visit[v[i][j].first] == false)//갈 수 있는곳이면
+		{
+			sum += v[i][j].second;
+			if (sum > max_num) {
+				max_num = sum;
+			}
+			dfs(v, (v[i][j].first), sum, visit); //랑 연결되어있는 수를 들어가면서 w를 더해줌
+			sum -= v[i][j].second;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	int x, y, weight;
+	vector<pair<int, int>>v[10001];
+	bool visit[10001] = { false, };
+	for (int i = 0; i < n - 1; i++)
+	{
+		cin >> x >> y >> weight;
+		v[x].push_back(make_pair(y, weight));
+		v[y].push_back(make_pair(x, weight));
+	}
+
+	for (int i = 1; i < n + 1; i++)
+	{
+		dfs(v, i, 0, visit);
+		memset(visit, false, sizeof(visit));
+	}
+	cout << max_num;
+	return 0;
+}
---
+++ BOJ/BOJ_9935 문자열 폭발.cpp
@@ -0,0 +1,58 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+#include<stack>
+#include<algorithm> 
+using namespace std;
+int main()
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    string s;
+    cin >> s;
+    string boom;
+    cin >> boom;
+    stack<char>st;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        st.push(s[i]);
+
+        if (s[i] == boom[boom.size() - 1] && st.size() >= boom.size())
+        {
+            string temp = "";
+            for (int j = 0; j < boom.length(); j++)
+            {
+                temp += st.top();
+                st.pop();
+            }
+            reverse(temp.begin(), temp.end());
+
+            if (temp != boom)
+            {
+                for (int j = 0; j < temp.length(); j++)
+                {
+                    st.push(temp[j]);
+                }
+            }
+        }
+    }
+
+    string answer;
+    if (st.empty())
+    {
+        cout << "FRULA";
+    }
+    else {
+        int size = st.size(); 
+        for (int i = 0; i < size; i++)
+        {
+            answer += st.top();
+            st.pop();
+        }
+        reverse(answer.begin(), answer.end());
+        cout << answer;
+    }
+    return 0;
+}
